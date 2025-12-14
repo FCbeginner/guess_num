4699edeef3129d0951c281e97fc5eeecf40c4f72
Fcbeginner <aaaaa110215@gmail.com>
2025-12-14 23:12:31 +0800

    修改輸出版面配置

--- my_version/main.cpp
+++ my_version/main.cpp
@@ -24,8 +24,10 @@ int main(){
         double times_w_2 = worst_Case_divide_three(n);
         double times_a_1 = simulation_avg(n, 2);
         double times_a_2 = simulation_avg(n, 3);
-        cout << "小華's version" << "\t" << times_w_1 << "\t" << times_a_1 << "\n";
-        cout << "小英's version" << "\t" << times_w_2 << "\t" << times_a_2 << "\n";
+        cout << "\t" << "worst cace" << "\t" << "average case" << "\n";
+        cout << "小華" << "\t" << times_w_1 << "\t\t" << times_a_1 << "\n";
+        cout << "小英" << "\t" << times_w_2 << "\t\t" << times_a_2 << "\n";
+        cout << "unit" << "\t" << "(times)" << "\t\t" << "(times)" << "\n";
         cout << "--------------------------------------------\n";
     }
     return 0;
